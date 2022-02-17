0151b28c4341bb262da663e9a7de351b2adb711e
Kang9256 <john921002@gmail.com>
2022-02-17 15:27:14 +0900

    2022.02.17

--- PayStory/src/main/java/com/AourZ/PayStory/controller/PublicAccountController.java
+++ PayStory/src/main/java/com/AourZ/PayStory/controller/PublicAccountController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class PublicAccountController {
-	@RequestMapping("/publicMain")
+	@RequestMapping("/publicmain")
 	public String movePublicMain() {
 		return "boardPublic/publicMain";
 	}
