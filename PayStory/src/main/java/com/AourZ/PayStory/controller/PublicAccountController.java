5d2346e4b4e74ac7b333d067de2ad7574c042b10
Kang9256 <john921002@gmail.com>
2022-02-18 14:00:15 +0900

    vo 간이 설정

--- PayStory/src/main/java/com/AourZ/PayStory/controller/PublicAccountController.java
+++ PayStory/src/main/java/com/AourZ/PayStory/controller/PublicAccountController.java
@@ -5,18 +5,28 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class PublicAccountController {
+	
+	//공유가계부 메인
 	@RequestMapping("/publicmain")
 	public String movePublicMain() {
 		return "boardPublic/publicMain";
 	}
 	
+	//공유가계부 생성
 	@RequestMapping("/test")
 	public String movePublicCreate() {
 		return "boardPublic/publicCreate";
 	}
 	
+	
+	
+	//공유가계부 참여자 등록,삭제
 	@RequestMapping("/publicRegisterParticipant")
 	public String moveRegisterParticipant() {
 		return "boardPublic/publicRegisterParticipant";
 	}
+	
+	
+	
+	
 }
