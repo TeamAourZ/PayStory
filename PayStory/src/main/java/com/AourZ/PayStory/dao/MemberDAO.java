51df63fa96d36f7934ce96a9392476fc7f2aaa3a
Reactionj <Reactionj@218.52.49.47>
2022-02-21 17:18:28 +0900

    (경로수정)

--- PayStory/src/main/java/com/AourZ/PayStory/dao/MemberDAO.java
+++ PayStory/src/main/java/com/AourZ/PayStory/dao/MemberDAO.java
@@ -20,18 +20,18 @@ public class MemberDAO implements IMemberDAO {
 	
 	@Override
 	public void register(MemberVO memberVO) throws Exception{
-		sqlsession.insert("com.AourZ.PayStory.dao.MemberDAO.register", memberVO);	
+		sqlsession.insert("com.AourZ.PayStory.dao.IMemberDAO.register", memberVO);	
 	}
 	
 	@Override
 	public int nameCnt(MemberVO memberVO)throws Exception{
-		return sqlsession.selectOne("com.AourZ.PayStory.dao.MemberDAO.nameCnt", memberVO);
+		return sqlsession.selectOne("com.AourZ.PayStory.dao.IMemberDAO.nameCnt", memberVO);
 	}
 	
 	@Override
 	public MemberVO login(LoginVO loginVO)throws Exception{
 		System.out.println("DAOloginVO"+loginVO.getMemberPwd());
-		return sqlsession.selectOne("com.AourZ.PayStory.dao.MemberDAO.login", loginVO);
+		return sqlsession.selectOne("com.AourZ.PayStory.dao.IMemberDAO.login", loginVO);
 	}
 	
 	@Override
@@ -40,19 +40,19 @@ public class MemberDAO implements IMemberDAO {
 		map.put("memberEmail", memberEmail);
 		map.put("authKey", authKey);
 		
-		sqlsession.selectOne("com.AourZ.PayStory.dao.MemberDAO.createAuthKey", map);
+		sqlsession.selectOne("com.AourZ.PayStory.dao.IMemberDAO.createAuthKey", map);
 		
 	}
 	
 	@Override
 	public void memberAuth(String memberEmail) throws Exception{
 
-		sqlsession.update("com.AourZ.PayStory.dao.MemberDAO.memberAuth", memberEmail);
+		sqlsession.update("com.AourZ.PayStory.dao.IMemberDAO.memberAuth", memberEmail);
 	}
 	
 	@Override
 	public int findPwCheck(MemberVO memberVO)throws Exception{
-	return sqlsession.selectOne("com.AourZ.PayStory.dao.MemberDAO.findPwCheck", memberVO);	
+	return sqlsession.selectOne("com.AourZ.PayStory.dao.IMemberDAO.findPwCheck", memberVO);	
 	}
 
 
@@ -62,7 +62,7 @@ public class MemberDAO implements IMemberDAO {
 		map.put("memberEmail", memberEmail);
 		map.put("memberName", memberName);
 		map.put("memberPwd", memberPwd);
-		return sqlsession.update("com.AourZ.PayStory.dao.MemberDAO.findPw", map);
+		return sqlsession.update("com.AourZ.PayStory.dao.IMemberDAO.findPw", map);
 	}
 	
 	@Override
@@ -70,12 +70,12 @@ public class MemberDAO implements IMemberDAO {
 		Map<String,Object> map = new HashMap<String, Object>();
 		map.put("memberName", memberVO.getMemberName());
 		
-	sqlsession.update("com.AourZ.PayStory.dao.MemberDAO.infoUpdate", map);
+	sqlsession.update("com.AourZ.PayStory.dao.IMemberDAO.infoUpdate", map);
 	}
 	
 	@Override
 	public String pwCheck(String memberEmail)throws Exception{
-		return sqlsession.selectOne("com.AourZ.PayStory.dao.MemberDAO.pwCheck", memberEmail);
+		return sqlsession.selectOne("com.AourZ.PayStory.dao.IMemberDAO.pwCheck", memberEmail);
 	}
 	
 	@Override
@@ -83,7 +83,7 @@ public class MemberDAO implements IMemberDAO {
 		Map<String,Object> map = new HashMap<String, Object>();
 		map.put("memberEmail", memberEmail);
 		map.put("memberPwd", hashedPw);
-		sqlsession.update("com.AourZ.PayStory.dao.MemberDAO.pwUpdate", map);
+		sqlsession.update("com.AourZ.PayStory.dao.IMemberDAO.pwUpdate", map);
 		
 	}
 	
