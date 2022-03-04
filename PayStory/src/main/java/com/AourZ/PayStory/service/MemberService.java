b82bdfbb6f3231b36a51c826a5422083a485b143
Hayoung Park <76716519+happai079@users.noreply.github.com>
2022-03-04 10:18:51 +0900

    [임시 비밀번호] ip 변경

--- PayStory/src/main/java/com/AourZ/PayStory/service/MemberService.java
+++ PayStory/src/main/java/com/AourZ/PayStory/service/MemberService.java
@@ -86,7 +86,7 @@ public class MemberService implements IMemberService {
 							"<br/>비밀번호 찾기를 통한 임시 비밀번호입니다."+
 							"<br/>임시비밀번호 :   <h2>"+memberKey+"</h2>"+
 							"<br/>로그인 후 비밀번호 변경을 해주세요."+
-							"<a href='http://localhost:8080/member/loginView"+
+							"<a href='http://49.50.174.115:8080/member/loginView"+
 							">로그인 페이지</a>");
 			sendMail.setFrom("paystory.aourz@gmail.com", "PayStory");
 			sendMail.setTo(memberEmail);
