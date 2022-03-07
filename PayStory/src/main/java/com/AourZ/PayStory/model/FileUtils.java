d92c443363b8def38d2d527764e2a11bed86d5d6
happai079 <gksyddl22@gmail.com>
2022-03-08 04:31:41 +0900

    [파일] 업로드 경로 재 설정

--- PayStory/src/main/java/com/AourZ/PayStory/model/FileUtils.java
+++ PayStory/src/main/java/com/AourZ/PayStory/model/FileUtils.java
@@ -13,8 +13,8 @@ import org.springframework.web.multipart.MultipartHttpServletRequest;
 
 @Component
 public class FileUtils {
-	// private static final String filePath = "/usr/local/tomcat9/webapps/ROOT/WEB-INF/classes/static/paystory/images/"; // 파일이 저장될 서버 위치
-	private static final String filePath = "C:/PayStory/images/"; // 파일이 저장될 서버 위치
+	private static final String filePath = "/usr/local/tomcat9/webapps/ROOT/WEB-INF/classes/static/paystory/images/"; // 파일이 저장될 서버 위치
+	// private static final String filePath = "C:/PayStory/images/"; // 파일이 저장될 서버 위치
 
 	public static String updateImg(
 			MultipartHttpServletRequest mpRequest, HttpSession session) throws Exception{
