d7b89b46d09cc87c58650ca29a7b70cbc897a386
Hyeon-2 <chamsae.kh@gmail.com>
2022-03-10 22:14:58 +0900

    모델 수정

--- PayStory/src/main/java/com/AourZ/PayStory/model/member/ShareMemberVO.java
+++ PayStory/src/main/java/com/AourZ/PayStory/model/member/ShareMemberVO.java
@@ -1,9 +1,14 @@
 package com.AourZ.PayStory.model.member;
 
 public class ShareMemberVO {
+	private String memberNo;			// 회원 번호
 	private String memberName;		// 회원 이름 (닉네임)
 	private String memberEmail;		// 회원 이메일 아이디
 	private String memberImage;	// 회원 프로필 이미지
+
+	public String getMemberNo() {
+		return memberNo;
+	}
 
 	public String getMemberName() {
 		return memberName;
@@ -15,6 +20,10 @@ public class ShareMemberVO {
 
 	public String getMemberImage() {
 		return memberImage;
+	}
+
+	public void setMemberNo(String memberNo) {
+		this.memberNo = memberNo;
 	}
 
 	public void setMemberName(String memberName) {
