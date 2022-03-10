6f99740c7b9104c66d9d7ac2846723b0fa3097d9
Hyeon-2 <chamsae.kh@gmail.com>
2022-03-10 22:15:33 +0900

    일부 코드 정리, 상세 내역 추가 및 수정 (임시 포함)

--- PayStory/src/main/java/com/AourZ/PayStory/dao/IAccountBookDAO.java
+++ PayStory/src/main/java/com/AourZ/PayStory/dao/IAccountBookDAO.java
@@ -20,17 +20,23 @@ public interface IAccountBookDAO {
 	AccountBookBudgetVO selectAccountBookBudget(HashMap<String, Object> map);	// 가계부 예산 조회
 	ArrayList<ShareAccountBookVO> selectShareMemberList(int accountBookNo);			// 공유 가계부 소유자, 참여자 조회
 	MemberVO selectMemberInfo(HashMap<String, Object> map);								// 회원 정보 조회
-
+	
+	IncomeVO selectIncome(HashMap<String, Object> map);				// 수입 내역 조회 (단일)
+	ExpenditureVO selectExpenditure(HashMap<String, Object> map);	// 지출 내역 조회 (단일)
+	
 	ArrayList<IncomeVO> selectIncomeList(HashMap<String, Object> map);						// 수입 내역 조회
 	ArrayList<ExpenditureVO> selectExpenditureList(HashMap<String, Object> map);			// 지출 내역 조회
-	void deleteItem(HashMap<String, Object> map);															// 내역 삭제
 	ArrayList<ExpenditureItemVO> selectExpenditureItem(int expenditureNo);						// 지출 상세 항목 조회
 	ArrayList<TagTotalVO> selectAccountBookTotalDataList(HashMap<String, Object> map);	// 수입 / 지출 총 건수, 총 금액
-	String selectTagName(String tagNo);																			// 태그 이름 조회
+	String selectTagInfo(HashMap<String, Object> map);													// 태그 번호 / 이름 조회
+	
+	void deleteItem(HashMap<String, Object> map);		// 내역 삭제
+	void updateItem(HashMap<String, Object> map);	// 내역 수정
+	void deleteDetailItem(int expenditureNo);				// 지출 상세 내역 삭제
 	
 	ArrayList<BoardVO> selectBoardList(HashMap<String, Object> map);// 게시판 게시글 리스트
 	String selectBoardCategoryName(String boardCategoryNo);				// 게시판 카테고리 이름 조회
-  
+	
 	public void insertIncome(IncomeVO vo); // 수입 항목 추가
 	public void insertExpenditure(ExpenditureVO vo); // 지출 항목 추가
 	public void insertExpenditureItem(ArrayList<ExpenditureItemVO> list);// 지출 아이템 추가
