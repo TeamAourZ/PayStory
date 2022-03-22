dd76ebf5c252db845eb008861926636eb40860ec
Hyeon-2 <chamsae.kh@gmail.com>
2022-03-22 09:48:53 +0900

    가계부 수정, 월별 예산 추가 또는 수정 ajax

--- PayStory/src/main/resources/static/main/js/ajax/budgetSettingAjax.js
+++ PayStory/src/main/resources/static/main/js/ajax/budgetSettingAjax.js
@@ -1,11 +1,23 @@
 /* 예산 설정 모달 - 추가 또는 수정 */
-function budgetSettingAjax() {
+function budgetSettingAjax(date, budget) {
 	$.ajax({
 		type: 'post',
 		url: "/accountBook/modal/budgetSetting",
 		data: {
+			"date": date,
+			"budget": budget
 		},
-		success: function(result) {
+		success: function() {
+			alert(date + " 의 예산 설정이 완료되었습니다.");
+
+			if (date === $('#firstModalDate').val()) {
+				$('#budget').val(budget.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ','));
+
+				budgetStatusAjax(nowYear, nowMonth);
+
+				$('#secondModalDate').val("");
+				$('#inputBudget').val("");
+			}
 		},
 		error: function() {
 			console.log("error");
