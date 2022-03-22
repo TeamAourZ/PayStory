9374b77c3d779e434e0bdb190b61a502e3d5e13c
Hyeon-2 <chamsae.kh@gmail.com>
2022-03-22 10:23:00 +0900

    변경된 타이틀 바로 반영되도록.

--- PayStory/src/main/resources/static/main/js/ajax/updateAccountBookInfoAjax.js
+++ PayStory/src/main/resources/static/main/js/ajax/updateAccountBookInfoAjax.js
@@ -9,6 +9,8 @@ function updateAccountBookInfoAjax(title, describe) {
 		},
 		success: function() {
 			alert("수정 내역이 반영되었습니다.");
+			
+			$('.accountBookTitle').text(title);
 		},
 		error: function() {
 			console.log("error");
