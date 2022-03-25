cb2351767d30e8b568378251aa83d2be82dd2407
Hyeon-2 <chamsae.kh@gmail.com>
2022-03-25 11:05:11 +0900

    경로 수정

--- PayStory/src/main/java/com/AourZ/PayStory/WebConfig.java
+++ PayStory/src/main/java/com/AourZ/PayStory/WebConfig.java
@@ -10,11 +10,13 @@ public class WebConfig implements WebMvcConfigurer {
 	@Override
 	public void addResourceHandlers(ResourceHandlerRegistry registry) {
 		registry.addResourceHandler("/images/**")
-		.addResourceLocations("file:///C:/PayStory/images/")
+//		.addResourceLocations("file:///C:/PayStory/images/")
+		.addResourceLocations("file:///usr/local/tomcat9/webapps/ROOT/WEB-INF/classes/static/paystory/images/")
 		.addResourceLocations("file:///upload/");
 		
 		registry.addResourceHandler("/voice/**")
-		.addResourceLocations("file:///C:/PayStory/upload/")
+//		.addResourceLocations("file:///C:/PayStory/upload/")
+		.addResourceLocations("file:///usr/local/tomcat9/webapps/ROOT/WEB-INF/classes/static/paystory/voices/")
 		.addResourceLocations("file:///upload/");
 		
 	}
